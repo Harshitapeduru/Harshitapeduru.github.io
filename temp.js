fc954cb16a6e6e9bed83e3abc4899d17526da6a7
Harshitapeduru <harshita.peduru@gmail.com>
2024-11-22 18:08:14 +0530

    modified

---
+++ temp.js
@@ -0,0 +1,40 @@
+db.products.insertMany(
+    [
+        {
+              "name":"Product 1",
+                  "desc":"This is the description of product 1",
+                  "price":45,
+                   "url":"https://picsum.photos/id/257/300/300"
+            },
+            {
+                    "name": "Product 2",
+                    "desc": "This is the description of product 2",
+                    "price": 90,
+                    "url": "https://picsum.photos/id/059/300/300"
+                },
+                {
+                    "name":"Product 3",
+                    "desc":"This is the description of product 3",
+                    "price":75,
+                    "url":"https://picsum.photos/id/045/300/300"
+                },
+                {
+                    "name":"Product 4",
+                    "desc":"This is the description of product 6",
+                    "price":59,
+                    "url":"https://picsum.photos/id/096/300/300"
+                },
+    {
+        "name": "Product 5",
+        "desc": "This is the description of product 5",
+        "price": 25,
+        "url": "https://picsum.photos/id/1410/300/300"
+    },
+    {
+        "name":"Product 6",
+        "desc":"This is the description of product 6",
+        "price":32,
+        "url":"https://picsum.photos/id/235/300/300"
+    }
+    ]
+);
